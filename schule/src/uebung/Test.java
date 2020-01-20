9875d9ba7a170731d3d103af2050296814aa2471
kirmailu <lukas.kirmaier@gmail.com>
2020-01-20 11:55:32 +0100

    Übung

---
+++ schule/src/uebung/Test.java
@@ -0,0 +1,18 @@
+package uebung;
+
+public class Test {
+
+	public Test() {
+		System.out.println("hallo world");
+	}
+
+	public void test() {
+		
+		
+		
+	}
+	public int add (int a, int b){
+		int ab = a + b;
+		return ab;
+	}
+}
